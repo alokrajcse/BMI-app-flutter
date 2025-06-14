db819b4c02c29a2a7fb51588d07e3621737a2c75
alokrajcse <cse.22bcse81@silicon.ac.in>
2025-06-14 12:12:18 +0530

    init

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'BMI  Calculator',
       theme: ThemeData(
 
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Rocket BMI Calculator'),
     );
   }
 }
